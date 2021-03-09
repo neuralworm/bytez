d12c3269635fc0f6d64f44bb0d5c49ba4c9bac9d
Jake <jakemillerdev@gmail.com>
2021-03-08 16:53:31 -0800

    v-0.1.0 removed if statements for finding the correct divisor and appendations index, and now uses finding the log10 of the number and dividing by 3.

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
+// appendations
+const appendationFactor = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+const bitsOrKibibits = ["", "i"]
+const bytesOrBits = ["B", "bit"]
+
+function returnAppendation(factor, bits = false, kibi = false) {
+  return (
+    appendationFactor[factor] +
+    bitsOrKibibits[kibi ? 1 : 0] +
+    bytesOrBits[bits ? 1 : 0]
+  )
+}
+
 module.exports = function (byteString, optionsObject) {
   // set base options
   const options = { ...defaultOptions, ...optionsObject }
-
   // set number and check type
   let number = byteString
-  if (typeof number !== "number") {
-    number = parseFloat(byteString)
-    console.log('type was string')
-  }
-
+  if (typeof number == "string") {
+    // handle empty string while converting to number type
+    number.length > 0 ? (number = parseFloat(byteString)) : 0
+  } else if (!Number.isFinite(number))
+    throw new TypeError(
+      "Number must be of type Number or String, received " + typeof number
+    )
   // find correct divisor
-  let divisor = 1
   let appendation = ""
-  if(number < Math.pow(10, 3)){
-      divisor = 1
-      appendation = "B"
-  }
-  else if(number < Math.pow(10, 6)){
-    divisor = Math.pow(10,3)
-    appendation = "kB"
-}
-else if(number < Math.pow(10,9)){
-    divisor = Math.pow(10,6)
-    appendation = 'mB'
-}
-else if(number < Math.pow(10,12)){
-    divisor = Math.pow(10,9)
-    appendation = 'gB'
-}
-else if(number < Math.pow(10,15)){
-    divisor = Math.pow(10,12)
-    appendation = 'tB'
-}
 
+  // get correct factor to determine appendation from array (divide by 3 for each 1k places)
+  let factor = number ? Math.floor(Math.log10(number) / 3) : 0
+  console.log(factor + " factor from number " + number)
+
+  divisor = Math.pow(10, factor * 3)
+  appendation = returnAppendation(factor)
 
   // conver with correct divisor, convert to string, and round it to nearest n(precision) digits.
   number = (number / divisor).toFixed(options.precision)
-    console.log(number)
   // determine if should leave digit on
-    if(options.roundOffInt && Number.isInteger(parseFloat(number))) number = number.split('.')[0]
+  if (options.roundOffInt && Number.isInteger(parseFloat(number)))
+    number = number.split(".")[0]
 
   return `${number}${appendation}`
 }
-
 const defaultOptions = {
-  casing: "camelCase",
   bits: false,
   kibibytes: false,
   precision: 1,
-  roundOffInt: true
+  roundOffInt: true,
 }
--- tests/convert.test.js
+++ tests/convert.test.js
@@ -1,22 +1,59 @@
-const convert = require('../index')
+const convert = require("../index")
 
-test('convert 1000 bytes NUMBER to readable string', () => {
-    expect(convert(1000, {
-        roundOffInt: true
-    })).toBe("1kB")
+test("convert 1000 bytes NUMBER to readable string", () => {
+  expect(
+    convert(1000, {
+      roundOffInt: true,
+    })
+  ).toBe("1KB")
 })
-test('convert 100 bytes NUMBER to readable string', () => {
-    expect(convert(100)).toBe("100B")
+test("convert 100 bytes NUMBER to readable string", () => {
+  expect(convert(100)).toBe("100B")
 })
-test('convert 10001 bytes NUMBER to readable string', () => {
-    expect(convert(10001)).toBe("10kB")
+test("convert 10001 bytes NUMBER to readable string", () => {
+  expect(convert(10001)).toBe("10KB")
 })
-test('convert 10001 bytes STRING to readable string', () => {
-    expect(convert('10001')).toBe("10kB")
+test("convert 10001 bytes STRING to readable string", () => {
+  expect(convert("10001")).toBe("10KB")
 })
-test('convert 1320000 bytes NUMBER to readable string', () => {
-    expect(convert(1320000)).toBe("1.3mB")
+test("convert 1320000 bytes NUMBER to readable string", () => {
+  expect(convert(1320000)).toBe("1.3MB")
 })
-test('convert 1320000 bytes STRING to readable string', () => {
-    expect(convert('1320000')).toBe("1.3mB")
+test("convert 1320000 bytes STRING to readable string", () => {
+  expect(convert("1320000")).toBe("1.3MB")
 })
+test('convert ""  STRING to readable string', () => {
+  expect(convert("")).toBe("0B")
+})
+test('convert "0.8.9.0"  STRING to readable string', () => {
+  expect(convert("0B")).toBe("0B")
+})
+test("convert 340918350984  NUMBER to readable string", () => {
+  expect(convert(340918350984)).toBe("340.9GB")
+})
+test("convert 12309823123123  NUMBER to readable string", () => {
+  expect(convert(12309823123123)).toBe("12.3TB")
+})
+test("convert 421230982312312344  NUMBER to readable string", () => {
+  expect(convert(421230982312312344)).toBe("421.2PB")
+})
+test("handle inserted OBJECT with error", () => {
+  expect(() => convert({ number: 5 })).toThrow()
+})
+test("handle null passed in", () => {
+    expect(() => convert(null)).toThrow()
+
+})
+test("handle undefined passed in", () => {
+    expect(() => convert(undefined)).toThrow()
+
+})
+test("handle boolean passed in", () => {
+    expect(() => convert(true)).toThrow()
+
+})
+test("handle infinity passed in", () => {
+    expect(() => convert(Infinity)).toThrow()
+
+})
+
