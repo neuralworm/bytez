ee9fa48a7500f287cf64cc322a33ae3a78a81e3a
Jake <jakemillerdev@gmail.com>
2021-03-15 22:11:05 -0700

    testing typings inclusion

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+declare const appendationFactor: string[];
+declare const bitsOrKibibits: string[];
+declare const bytesOrBits: string[];
+declare const defaultOptions: {
+    bits: boolean;
+    base2: boolean;
+    precision: number;
+    roundOffInt: boolean;
+};
